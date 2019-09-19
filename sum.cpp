0d87a509a9e117beb220f5f2f3faac944e37fd33
cappitio <IOcappit>
2019-09-19 16:50:27 +0900

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,9 +1,5 @@
 #include "sum.h" // <> vs "" -> 시스템 헤더 파일 vs 현재 폴더에 있는 파일
 
 int sum(int n) {
-    int result = 0;
-    for(int i = 0; i < n; i++){
-        result += (i + 1);
-    }
-    return result;
+    return n * (n + 1) / 2;
 }
